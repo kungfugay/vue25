eab7fffe264004ee68f4e57755fde67d629b4d34
kungfugay <paidthatmoney@gmail.com>
2025-06-11 17:32:36 +0900

    2025.06.11 - 5 : Slot 2

--- scalingup/comp/src/App.vue
+++ scalingup/comp/src/App.vue
@@ -1,7 +1,31 @@
 <template>
-  <slot-comp>Hello World!</slot-comp>
+  <slot-comp v-for="x in foods">
+    <h4>{{ x.name }}</h4>
+    <p>{{ x.desc }}</p>
+  </slot-comp>
+  <footer>
+    <slot-comp>
+      <h3>Footer</h3>
+    </slot-comp>
+  </footer>
+  <slot-comp></slot-comp>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      foods: [
+        { name: "pizza", desc: "good" },
+        { name: "coke", desc: "bad" },
+      ],
+    };
+  },
+};
+</script>
 
-<style scoped></style>
+<style scoped>
+footer > div {
+  background-color: lightpink;
+}
+</style>
--- scalingup/comp/src/components/SlotComp.vue
+++ scalingup/comp/src/components/SlotComp.vue
@@ -1,4 +1,24 @@
 <template>
-  <div>SlotComp.vue</div>
-  <slot></slot>
+  <!-- slot 태그 위에 div를 넣으면 card처럼 쓸 수 있음 -->
+  <div>
+    <slot>
+      <h4>기본으로 제공되는 fallback 컨텐츠</h4>
+    </slot>
+  </div>
 </template>
+
+<style scoped>
+div {
+  background-color: lightgreen;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  flex-basis: 150px;
+  border-radius: 10px;
+  border: solid black 2px;
+  margin: 10px;
+  padding: 20px 10px 0 10px;
+}
+div:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
