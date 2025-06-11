fc00d61152538a6f5a644324fa516217567afb3e
kungfugay <paidthatmoney@gmail.com>
2025-06-11 16:12:44 +0900

    2025.06.11 - 1 : v-for components

--- scalingup/firstsfc/src/App.vue
+++ scalingup/firstsfc/src/App.vue
@@ -1,21 +1,51 @@
 <template>
   <h1>Food</h1>
   <div id="wrapper">
+    <!-- 컴포넌트를 v-for로 사용 -->
     <food-item
-      food-name="Apples"
-      food-desc="Apples are a type of fruit that grow on trees."
-      v-bind:is-favorite="true"
+      v-for="x in foods"
+      :food-name="x.name"
+      :food-desc="x.desc"
+      :is-favorite="x.favorite"
     />
-    <food-item
-      food-name="Pizza"
-      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."
-      v-bind:is-favorite="false"
-    />
-    <food-item food-name="Rice" v-bind:is-favorite="false" />
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      foods: [
+        {
+          name: "Apples",
+          desc: "Apples are a type of fruit that grow on trees.",
+          favorite: true,
+        },
+        {
+          name: "Pizza",
+          desc: "Pizza has a bread base with tomato sauce, cheese, and toppings on top.",
+          favorite: false,
+        },
+        {
+          name: "Rice",
+          desc: "Rice is a type of grain that people like to eat.",
+          favorite: false,
+        },
+        {
+          name: "Fish",
+          desc: "Fish is an animal that lives in water.",
+          favorite: true,
+        },
+        {
+          name: "Cake",
+          desc: "Cake is something sweet that tastes good.",
+          favorite: false,
+        },
+      ],
+    };
+  },
+};
+</script>
 
 <style>
 #wrapper {
@@ -24,6 +54,7 @@
 }
 #wrapper > div {
   border: dashed black 1px;
+  flex-basis: 120px;
   margin: 10px;
   padding: 10px;
   background-color: lightgreen;
--- scalingup/firstsfc/src/components/FoodItem.vue
+++ scalingup/firstsfc/src/components/FoodItem.vue
@@ -2,41 +2,16 @@
   <div>
     <h2>
       {{ foodName }}
-      <img src="/img_quality.svg" alt="" v-show="foodIsFavorite" />
+      <img src="/img_quality.svg" v-show="foodIsFavorite" />
     </h2>
     <p>{{ foodDesc }}</p>
-    <button @:click="toggleFavorite">Favorite</button>
+    <button v-on:click="toggleFavorite">Favorite</button>
   </div>
 </template>
 
 <script>
 export default {
-  // props: ['foodName','foodDesc','isFavorite']
-  // object로 받기 (타입 정의)
-  props: {
-    foodName: {
-      type: String,
-      required: true, // required 속성 추가
-    },
-    foodDesc: {
-      type: String,
-      required: false,
-      default: "This is the default description.", // props의 기본값을 설정할 수 있습니다.
-      validator: function (value) {
-        // 유효성 검사기를 추가 가능하다. -> 경고를 console 창에서 확인 가능
-        if (20 < value.length && value.length < 50) {
-          return true;
-        } else {
-          return false;
-        }
-      },
-    },
-    isFavorite: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-  },
+  props: ["foodName", "foodDesc", "isFavorite"],
   data() {
     return {
       foodIsFavorite: this.isFavorite,
@@ -50,7 +25,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 img {
   height: 1.5em;
   float: right;
