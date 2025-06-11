4e9ef50572464da88dd7bd71bdf0430dfcef6f10
kungfugay <paidthatmoney@gmail.com>
2025-06-11 17:20:16 +0900

    2025.06.11 - 4 : Local Components

---
+++ scalingup/comp/src/components/CompOne.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script>
+export default {
+  name: "App", // 파일 이름을 컴포넌트 이름으로 자동 삽입
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
