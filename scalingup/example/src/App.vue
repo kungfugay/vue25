fc5858e88e97dd0c7dde7f0caa8f65905cab9566
kungfugay <paidthatmoney@gmail.com>
2025-06-12 16:04:52 +0900

    2025.06.12 - 1 : http 요청 연습

---
+++ scalingup/example/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @:click="fetchData">데이터 가져오기</button>
+    <ul v-if="data">
+      <li v-for="(user, index) in data.data" :key="index">
+        <div v-for="key in Object.keys(user)" :key="key">
+          {{ key }}: {{ user[key] }}
+        </div>
+      </li>
+    </ul>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      data: null,
+    };
+  },
+  methods: {
+    async fetchData() {
+      this.data = await axios.get("https://jsonplaceholder.typicode.com/users");
+    },
+  },
+};
+</script>
